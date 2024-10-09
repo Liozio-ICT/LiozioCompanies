7881e8a04ea696e0e9f1b871f3b27ec5528ac94c
Liozio-IT <desmond@liozio.com>
2024-10-09 16:00:54 +0100

    Fixed URL paths

--- LiozioMainApp/urls.py
+++ LiozioMainApp/urls.py
@@ -4,13 +4,13 @@ from django.urls import path # type: ignore
 from . import views
 urlpatterns = [
     path('',views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('about', views.about, name='about'),
     # path('about/<str:about_var>', views.about_more, name='about-more'),
-    path('contact/', views.contact, name='contact'),
-    path('services/', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
+    path('services', views.services, name='services'),
     # path('special/<slug:slug>', views.sp_service, name='services-special'),
-    path('careers/', views.careers, name='careers'),
+    path('careers', views.careers, name='careers'),
     path('careers/<slug:slug>', views.career, name='career-detail'),
-    path('projects/', views.projects, name='projects'),
+    path('projects', views.projects, name='projects'),
     path('projects/<slug:slug>', views.project, name='project-detail'),
 ]
